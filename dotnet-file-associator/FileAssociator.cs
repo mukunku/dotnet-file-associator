b109db3ec8321dba14d489e7b1ad2ff58ad74de1
Sal <chunkymonkey_42@hotmail.com>
2025-09-09 13:36:44 -0400

    cleanup usings

--- dotnet-file-associator/FileAssociator.cs
+++ dotnet-file-associator/FileAssociator.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Security.Principal;
 
 namespace DotnetFileAssociator
 {
